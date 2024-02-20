0bb7952915818c8da7577cffcfbc9f3f88d4e820
theFutureGuy <ashiqgit789@gmail.com>
2024-02-20 22:40:34 +0530

    jquery changes to removal

--- app.js
+++ app.js
@@ -1,22 +1,39 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const methodOverride = require('method-override');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const myrouter = require('./routers/router');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const flash = require('connect-flash');
 
-app.get("/", (req,res) => {
-    console.log("Root");
-    res.send("hello root");
-})
+dotenv.config();
+mongoose.connect(process.env.MONGODBURL);
+app.set('view engine', 'ejs');
 
+app.use(methodOverride('_method'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-app.get('/employee',(req,res) =>{
-    var emp1 = {
-        fname:'ashiq',
-        lname:'a',
-        emp_type:'CEO',
-        salary:'6b'
-    }
-    res.send("update")
-})
+// session middleware
+app.use(session({
+    secret: 'nodejs',
+    resave: true,
+    saveUninitialized: true
+}));
 
-app.listen(3000,() => {
-    console.log("Server is listening at 3000");
-})
+// flash middleware
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.err = req.flash('err');
+    next();
+});
+
+app.use(myrouter);
+
+app.listen(process.env.PORT, () => {
+    console.log(process.env.PORT, "Port Working");
+});
