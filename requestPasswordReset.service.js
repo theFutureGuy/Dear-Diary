662d336ea180f55b4236d71870c4d734613ca0e6
theFutureGuy <ashiqgit789@gmail.com>
2024-03-02 11:03:42 +0530

    changes patch

---
+++ requestPasswordReset.service.js
@@ -0,0 +1,47 @@
+const User = require("./models/User.model.js");
+const Token = require("./models/Token.model.js");
+const crypto = require("crypto");
+const bcrypt = require("bcrypt");
+const sendEmail = require("./utils/sendEmail.js");
+
+require("dotenv").config();
+const clientURL = process.env.CLIENT_URL;
+
+const requestPasswordReset = async (email) => {
+  const user = await User.findOne({ email });
+  if (!user) throw new Error("User does not exist");
+
+  let token = await Token.findOne({ userId: user._id });
+
+  if (token) {
+    await token.deleteOne();
+  }
+  let resetToken = crypto.randomBytes(32).toString("hex");
+  const bcryptSalt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(resetToken, Number(bcryptSalt));
+
+  await new Token({
+    userId: user._id,
+    token: hash,
+    createdAt: Date.now(),
+  }).save();
+
+  const link = `${clientURL}/password-reset?token=${encodeURIComponent(
+    resetToken
+  )}&id=${encodeURIComponent(user._id)}`;
+  sendEmail(
+    user.email,
+    "Password Reset Request",
+    {
+      name: user.email,
+      userId: encodeURIComponent(user.id),
+      link: link,
+      clientURL: clientURL,
+      resetToken: resetToken,
+    },
+    "../template/requestPasswordReset.ejs"
+  );
+  return { link };
+};
+
+module.exports = requestPasswordReset;
---
+++ resetPassword.service.js
@@ -0,0 +1,40 @@
+const sendEmail = require("./utils/sendEmail.js");
+const Token = require("./models/Token.model.js");
+const User = require("./models/User.model.js");
+const bcrypt = require("bcrypt");
+
+const resetPassword = async (userId, token, password) => {
+  let passwordResetToken = await Token.findOne({ userId });
+
+  if (!passwordResetToken) {
+    throw new Error("Invalid or expired password reset token");
+  }
+  // console.log(passwordResetToken.token, token); 
+
+  const isValid = await bcrypt.compare(token, passwordResetToken.token);
+  if (!isValid) throw new Error("Invalid or expired password reset token");
+
+  const bcryptSalt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, Number(bcryptSalt));
+
+  await User.updateOne(
+    { _id: userId },
+    { $set: { password: hash } },
+    { new: true }
+  );
+
+  const user = await User.findById({ _id: userId });
+  sendEmail(
+    user.email,
+    "Password Reset Successfully",
+    {
+      name: user.email,
+    },
+    "../template/resetPassword.ejs"
+  );
+  await passwordResetToken.deleteOne();
+
+  return { message: "Password reset was successful" };
+};
+
+module.exports = resetPassword;
