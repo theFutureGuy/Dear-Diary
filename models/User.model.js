9f5fd2878fcf19fc71b424949d56c43464c91452
theFutureGuy <ashiqgit789@gmail.com>
2024-03-02 11:04:50 +0530

    jwt token enables and user schema

---
+++ models/User.model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    validate: {
+      validator: async function (value) {
+        const user = await this.constructor.findOne({ email: value });
+        return !user;
+      },
+      message: "Email already exists",
+    },
+    validate: {
+      validator: (value) => validator.isEmail(value),
+      message: "Invalid email format",
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+
+module.exports = mongoose.model("User", userSchema);
