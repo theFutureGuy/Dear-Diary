739d06eff8bcee46e2e806d46059a21ca322389b
theFutureGuy <ashiqgit789@gmail.com>
2024-03-02 11:02:55 +0530

    Journalling Auth

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const app = express();
+const connectDB = require("./db");
+require("dotenv").config();
+
+const port = process.env.PORT || 3000;
+const cookieParser = require("cookie-parser");
+const methodOverride = require("method-override");
+
+// import route files
+const loginRoute = require("./routes/login.js");
+const signupRoute = require("./routes/signup.js");
+const logoutRoute = require("./routes/logout.js");
+const journalRoute = require("./routes/journals.js");
+const authenticateToken = require("./auth.js");
+const createJournalRoute = require("./routes/create-journal.js");
+const viewJournalRoute = require("./routes/view-journal.js");
+const deleteJournalRoute = require("./routes/delete-journal.js");
+const editJournalRoute = require("./routes/edit-journal.js");
+const profileChange = require("./routes/profile-change.js");
+const passwordResetRoute = require("./routes/password-reset.js");
+const shareableLink = require("./routes/public-link");
+
+// Configure body-parser middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(methodOverride("_method"));
+
+connectDB(); // Database connection
+
+// Register view engine
+app.set("view engine", "ejs");
+app.set("template", "./template");
+
+// app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+// Mount the login routes
+app.use("/login", loginRoute);
+// Mount the signup routes
+app.use("/signup", signupRoute);
+app.use("/journals", authenticateToken, journalRoute);
+app.use("/journals/", authenticateToken, createJournalRoute);
+app.use("/", logoutRoute);
+app.use("/journals", authenticateToken);
+app.use("/journals", viewJournalRoute);
+app.use("/journals", authenticateToken, deleteJournalRoute);
+app.use("/journals", authenticateToken, editJournalRoute);
+app.use("/", authenticateToken, profileChange);
+app.use("/", passwordResetRoute);
+app.use("/", shareableLink);
+app.listen(port);
---
+++ routes/signup.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const User = require('../models/User.model');
+
+router.get('/', (req, res) => {
+  res.render('signup', { title: 'Sign Up', emailError: null, passError : null });
+});
+
+router.post('/', async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.render('signup', {
+        emailError: 'Email already exists',
+        title: 'Sign Up',
+        passError : null
+      }) }
+    if(password.length < 8) {
+      return res.render('signup', {
+        title: 'Sign Up',
+        passError: 'Password must be at least 8 characters long',
+        emailError : null
+      })
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = new User({ email, password: hashedPassword });
+    await newUser.save();
+    res.redirect('/login')
+  } catch (error) {
+    console.error(error);
+    res.redirect('/signup');
+  }
+});
+
+module.exports = router;
