a21055ca86801d50b13db510c63f35eedd9e4bb6
theFutureGuy <ashiqgit789@gmail.com>
2024-03-02 11:05:39 +0530

    crud journel

---
+++ models/Journal.model.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+const slugify = require("slugify");
+
+const journalSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    summary: {
+      type: String,
+    },
+    markdown: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    isPublic: {
+      type: Boolean,
+      default: false,
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+journalSchema.pre("validate", function (next) {
+  if (this.title) {
+    this.slug = slugify(this.title, { lower: true, strict: true });
+  }
+
+  journalSchema.pre("findOneAndUpdate", async function (next) {
+    try {
+      const update = this.getUpdate();
+      if (update.title) {
+        const slug = slugify(update.title, { lower: true, strict: true });
+        this._update.slug = slug;
+      }
+      next();
+    } catch (error) {
+      next(error);
+    }
+  });
+  next();
+});
+
+module.exports = mongoose.model("Journal", journalSchema);
---
+++ routes/delete-journal.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const Journal = require('../models/Journal.model')
+
+router.delete('/:id', async (req, res) => {
+    try {
+    await Journal.findByIdAndDelete(req.params.id)
+    res.status(200).send();
+    } catch (error) {
+        console.log(error)
+        res.status(500)
+    }
+})
+
+module.exports = router;
